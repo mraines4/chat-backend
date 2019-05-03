38012ad686e280e8c4a87bfcb13defd4c31108a9
Matt Raines <mraines4@gmail.com>
2019-05-03 13:59:11 -0400

    adds websocket abilities

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+const http = require('http');
 const express = require('express');
 const app = express();
+const server = http.createServer(app); // create a plain http server that uses our express app
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({
+    server,        // piggybacking on the plain http server
+    path: '/chat'  // listen on only one route, allowing express to continue to listen on its custom routes
+});
+
 app.use(express.urlencoded({extended: true}));
 
 // This is my "database"
 const db = [
     "Welcome to the chat app. We've got fun and games"
 ];
+
+wss.on('connection', (socket) => {
+    console.log('heres a new connection!');
+    socket.send(JSON.stringify(db));
+
+    socket.on('message', (data) => {
+        console.log(data);
+        db.push(data);
+        console.log(db);
+        wss.clients.forEach((client) => {
+            if(client.readyState === WebSocket.OPEN){
+                client.send(JSON.stringify(data));
+            }
+        });
+        // socket.send(data);
+    });
+});
 
 // When GET request comes in, send back all the messages
 app.get('/api', (req, res) => {
@@ -21,4 +47,4 @@ app.post('/api', (req,res) => {
     })
 })
 
-app.listen(3010,() => {console.log(`lstng to musik`);});
+server.listen(3010,() => {console.log(`lstng to musik`);});
